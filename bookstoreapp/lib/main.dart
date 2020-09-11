377bc441c3b229c5a629cd4a5d105489205acb02
xenacro <kayush054@gmail.com>
2020-09-11 17:15:54 +0530

    Log in & sign up pages added

--- bookstoreapp/lib/main.dart
+++ bookstoreapp/lib/main.dart
@@ -1,4 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:bookstoreapp/pages/bookstoreapp.dart';
+import 'login_screen.dart';
 
-void main()=>runApp(ReaderStop());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey[250],
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        textTheme: TextTheme(
+          display2: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            fontFamily: 'OpenSans',
+          ),
+          subhead: TextStyle(fontFamily: 'NotoSans'),
+          body1: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+
+
+
